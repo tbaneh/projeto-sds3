87b32c33f21ae706b53c6731761555bf22e69b81
Fernando Augusto <7902228@gmail.com>
2021-05-07 17:25:17 -0300

    BarChart integration

--- frontend/src/components/DonutCharts/index.tsx
+++ frontend/src/components/DonutCharts/index.tsx
@@ -25,11 +25,6 @@ const DonutCharts = () => {
 
     }, [])
 
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
-
     const options = {
         legend: {
             show: true
